dd6a9ff958f71cb9cfeb933a663eccadf45e74ba
MauFus <mauro.fustinoni@gmail.com>
2013-10-31 16:39:40 +0100

    cambio nome interfaccia DBReceiver e aggiunta javadoc

--- PopT Common/src/popt/rmi/DBReceiver.java
+++ PopT Common/src/popt/rmi/DBReceiver.java
@@ -14,11 +14,27 @@ import java.rmi.RemoteException;
 
 import popt.data.Movie;
 
-public interface DBReceiverInterface extends Remote {
+public interface DBReceiver extends Remote {
 
+	/**
+	 * Invoca l'inserimento in DB del film
+	 * @param movie
+	 * @return il successo o meno dell'azione
+	 * @throws RemoteException
+	 */
 	public boolean insertMovie(Movie movie) throws RemoteException;
 
+	/**
+	 * Comunica se il DBReceiver è occupato o disponibile
+	 * @return lo stato occupato o meno
+	 * @throws RemoteException
+	 */
 	public boolean isAvailable() throws RemoteException;
 
+	/**
+	 * Fornisce il messaggio relativo allo stato del Receiver
+	 * @return lo stato del Receiver
+	 * @throws RemoteException
+	 */
 	public String getStatus() throws RemoteException;
 }
