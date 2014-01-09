3c2d4fcd4d33d6f7ee207dbd7c6d06c35fcbb5a3
Matteo Ronchi <mat.ronchi@gmail.com>
2014-01-09 17:56:07 +0100

    Implementazione dei MovieRect

--- PopT Scheduling/src/popt/gui_sch/MovieLine.java
+++ PopT Scheduling/src/popt/gui_sch/MovieLine.java
@@ -1,6 +1,9 @@
 package popt.gui_sch;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 public class MovieLine extends JPanel {
 
@@ -8,59 +11,18 @@ public class MovieLine extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 5314001806476548519L;
-	
-	/*private static class RectDraw extends JPanel {
-       
-		private static final long serialVersionUID = -3945719460841628833L;
-		private static final int PREF_H = 80;
-		private static final int PREF_W = 230;
-		protected void paintComponent(Graphics g) {
-        super.paintComponent(g);  
-         g.drawRect(230,80,10,10);  
-         g.setColor(Color.BLUE);  
-         g.fillRect(230,80,10,10);  
-        }
-        public Dimension getPreferredSize() {
-            return new Dimension(PREF_W, PREF_H); // appropriate constants
-          }
-        
-        }
-	
-	JPanel movieLinePanel = new JPanel();
-	RectDraw newrect = new RectDraw();
-	MovieRectImpl m;
+
 	
 	public MovieLine() {
-		newrect.setBackground(Color.BLUE);
-        movieLinePanel.add(newrect);
+		MovieRect r = new MovieRect(90, 50,(new Color(100,0,0)));
+		MovieRect r1 = new MovieRect(120, 50,(new Color(0,100,0)));
+		add(r);
+		add(r1);
+		SpringLayout springLayout = new SpringLayout();
+		springLayout.putConstraint(SpringLayout.NORTH, r, 25, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, r, 10, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, r1, 25, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, r1, 155, SpringLayout.WEST, this);
+		setLayout(springLayout);
 	}
-
-	
-
-	    private static class ButtonHandler implements ActionListener {
-	        public void actionPerformed(ActionEvent e) {
-	        System.exit(0);
-	        }
-	        }*/
-	    
-	    /*public static void main(String[] args) {
-	        JPanel content = new JPanel();
-	        RectDraw newrect= new RectDraw();
-	        newrect.setBackground(Color.RED);
-	        JButton okButton= new JButton("OK");
-	        JButton clearButton= new JButton("Clear");
-	        ButtonHandler listener= new ButtonHandler();
-	        okButton.addActionListener(listener);
-	        clearButton.addActionListener(listener);
-	        content.add(okButton);
-	        content.add(clearButton);
-	        content.add(newrect);
-	        JFrame window = new JFrame("GUI Test");
-	        window.setContentPane(content);
-	        window.setSize(720,100);
-	        window.setLocation(100,100);
-	        window.setVisible(true);
-	        }*/
-
-	    }
-
+}
--- PopT Scheduling/src/popt/gui_sch/MovieRect.java
+++ PopT Scheduling/src/popt/gui_sch/MovieRect.java
@@ -1,26 +1,124 @@
 package popt.gui_sch;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public interface MovieRect {
+import javax.swing.JTextPane;
 
-	  // restituiscono lunghezza e larghezza
-	  public int getLength();
-	  public int getWidth();
-	  // assegnano nuova lunghezza e larghezza
-	  public void setLength (int l);
-	  public void setWidth (int w);
+import sun.security.util.Length;
 
-	  // restituisce il colore
-	  public Color getColor();
-	  // assegna un nuovo colore
-	  public void setColor(Color c);
+public class MovieRect extends JTextPane {
 
-	  // restituiscono le coordinate dell'angolo di x ed y minima del rettangolo, chiamato "punto di aggancio"
-	  public int getMinX();
-	  public int getMinY();
-	  // assegnano nuove coordinate all'angolo di x ed y minima
-	  public void setMinX(int x);
-	  public void setMinY(int y);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1214497401382184679L;
+	private int xMin, yMin, xMax, yMax, xOld, yOld, xNew;
+	private Dimension size;
+	private int startTime, finishTime;
+	private Color color;
+	
+	public MovieRect(int w, int h, Color c) {
+
+		size = new Dimension(w,h);
+		setPreferredSize(size);
+		this.setEditable(false);
+		color = c;
+		this.setBackground(color);
+		this.setFocusable(false);
+		this.setToolTipText("Movie Title");
+		this.setText("Moviaamoawbdj");
+		this.setDragEnabled(true);
+		
+
+		this.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				//System.out.println("mouse released!");
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//System.out.println("mouse pressed!");
+				xOld = e.getX();
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				setBackground(color);
+				//setText("Movie Title");
+				//System.out.println("mouse exited!");
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				setBackground(new Color(color.getRed()+100,color.getGreen()+100,color.getBlue()+100));
+				//setText("s: "+getStartTime()+"f: "+getFinishTime() );
+				//System.out.println("mouse entered!");
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//System.out.println("mouse clicked!");
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		this.addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				//System.out.println("mouse moved!");
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				//System.out.println("mouse dragged!");
+		            xNew = e.getX();
+		            setPosition(getxMin()+xNew-xOld,getyMin());
+		            //startTime=getStartTime();
+		            //finishTime=getFinishTime();
+		            //setText("s: "+getStartTime()+"f: "+getFinishTime() );
+		            //xNew = getxMin()+xNew-xOld;
+		            repaint();
+			}
+		});
+	}
+
+	public void setPosition(int x, int y) {
+		this.setLocation(x, y);
+	}
+
+	public int getxMin() {
+		System.out.println("x min is:" + this.getX());
+		return this.getX();
+	}
+
+	public int getyMin() {
+		return this.getY();
+	}
+
+	public int getxMax() {
+		return (this.getxMin() + this.size.width);
+	}
+
+	public int getyMax() {
+		return (this.getyMin() - this.size.height);
+	}
+
+	public int getStartTime() {
+		return getxMin()+xNew-xOld;
+	}
+
+	public int getFinishTime() {
+		return (getxMin()+xNew-xOld)+this.size.width;
+	}
 	
 }
